eda0ae1cd19be3da34145f2cced24f2d6d3faad8
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2021-11-30 15:07:39 +0000

    Deploying to docs from @ books-are-next/bajecna-leta-pod-psa@9c90066f07de22d9453db02727e58d73deec878e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-6d3050b';
+const CACHE = 'cache-9c90066';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
